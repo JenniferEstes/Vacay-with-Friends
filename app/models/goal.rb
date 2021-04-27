a16d54e5d0ac038cd4c6eb1144faa176807eebbb
Jennifer <jenndestes@gmail.com>
2021-04-27 13:18:17 -0500

    Added Bcrpy gem

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,4 @@
 class Goal < ApplicationRecord
     belongs_to :user 
-    has_many :users, through :vacations
+    has_many :users, through: :vacations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :vacations
     has_many :goals
-    has_many :group_goals, through :vacations, source: :goal
+    has_many :group_goals, through: :vacations, source: :goal
+
+    has_secure_password
 end

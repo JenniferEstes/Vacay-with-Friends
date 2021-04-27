b42507b46f305aa389d8beac9743e4031319da1e
Jennifer <jenndestes@gmail.com>
2021-04-27 12:46:28 -0500

    Changed group_goals relationsohip in user model to include a source

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :vacations
     has_many :goals
-    has_many :group_goals, through :vacations
+    has_many :group_goals, through :vacations, source: :goal
 end

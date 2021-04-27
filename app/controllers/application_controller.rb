0290c4b070b0116dd41423fefbae223197ba5881
Jennifer <jenndestes@gmail.com>
2021-04-26 22:08:03 -0500

    Added Google Authentication route and created sessions controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    def create
+        session[:username] = params[:username]
+        redirect_to '/'
+
+        def destroy
+            session.delete :username
+          end 
+    end
+    
 end

1b16090c9f68551889ca9a014bca6d8beb00a739
Jennifer <jenndestes@gmail.com>
2021-04-27 12:26:43 -0500

    Filled in user and goal relationships

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,4 @@
 class Goal < ApplicationRecord
+    belongs_to :user 
+    has_many :users, through :vacations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :vacations
+    has_many :goals
+    has_many :group_goals, through :vacations
 end

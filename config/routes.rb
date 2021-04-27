d272567d53a1b55a8047ce7d7394bca98f660f00
Jennifer <jenndestes@gmail.com>
2021-04-27 13:37:40 -0500

    Added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :goals
-  resources :users
+  resources :users, only: [:new, :create]
   resources :vacations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/auth/google_oauth2/callback', to: 'sessions#omniauth'

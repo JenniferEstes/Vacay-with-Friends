0fc4d132aeac5adcbb0340cca66c6b677bf13427
Jennifer <jenndestes@gmail.com>
2021-04-27 15:16:23 -0500

    Set up new, create, and show routes for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+
+
   resources :goals
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :vacations
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 end

3b22fecb077c789bd45cc6f0e7a2f4ed1070dea3
Jennifer <jenndestes@gmail.com>
2021-04-27 20:08:15 -0500

    Added first and last name to user table.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  root "sessions#home"
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'user#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :goals
   resources :users, only: [:new, :create, :show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_171105) do
+ActiveRecord::Schema.define(version: 2021_04_28_010430) do
 
   create_table "goals", force: :cascade do |t|
     t.decimal "amount"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_171105) do
     t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
   end
 
   create_table "vacations", force: :cascade do |t|

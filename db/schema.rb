9665ab7c22c684b3fe7f839792522036335f24b6
Jennifer <jenndestes@gmail.com>
2021-04-27 20:08:40 -0500

    Migrated user table changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_010430) do
+ActiveRecord::Schema.define(version: 2021_04_28_010749) do
 
   create_table "goals", force: :cascade do |t|
     t.decimal "amount"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_010430) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "vacations", force: :cascade do |t|

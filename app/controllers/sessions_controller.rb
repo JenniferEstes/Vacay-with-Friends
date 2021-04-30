f2139488cc314ae63872e5a8871ebe7faec99cf8
Jennifer <jenndestes@gmail.com>
2021-04-30 12:44:27 -0500

    Updated Sessions Controller create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:user][:email])
-        if user && @user.authenticate(params[:user][:password])
+        if user && user.authenticate(params[:user][:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
         else

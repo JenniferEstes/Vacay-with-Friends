aaa754f5f74aa8b8197a8310beb33c542eff9a02
Jennifer <jenndestes@gmail.com>
2021-05-01 15:52:54 -0500

    Changed users_groups table to groups_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_045529) do
+ActiveRecord::Schema.define(version: 2021_05_01_203836) do
 
   create_table "goals", force: :cascade do |t|
     t.decimal "amount"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_05_01_045529) do
     t.string "name"
   end
 
+  create_table "groups_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -36,13 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_01_045529) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-  end
-
-  create_table "users_groups", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "group_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "vacations", force: :cascade do |t|

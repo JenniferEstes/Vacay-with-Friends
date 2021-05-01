2e740c76e18428a06a82691b75c1c61a8f387fe5
Jennifer <jenndestes@gmail.com>
2021-04-30 22:23:12 -0500

    Added CRUD methods to Goal controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,44 +1,46 @@
 class GoalsController < ApplicationController
-
-    def index 
-    @goals = current_user.goals
-    end
-
+#
+#     def index
+#     @goals = current_user.goals
+#     end
+#
     def new
         @goal = Goal.new
-    end 
-
+    end
+#
     def create
-        @goal = current_user.goals.build(goal_params)
+        @user = current_user
+        @goals = @user.goals
+        @goal = @goals.build(goal_params)
         if @goal.save
             redirect_to vacation_goals_path(@goal)
         else
             render :'/goals/new'
         end
     end
-
-    def show
-        @goal = Goal.find_by(id: params[:id])
-    end
-
-    def update
-        @goal.update(goal_params)
-        redirect_to goal_vacation_path(@goal)
-    end
-
-    def update
-        @goal.update(goal_params)
-        redirect_to goal_vacation_path(@goal)
-    end 
-    
-    def destroy
-        @goal.destroy
-        flash[:notice] = "Goal deleted."
-        redirect_to goals_path
-    end 
-
-    private 
+#
+#     def show
+#         @goal = Goal.find_by(id: params[:id])
+#     end
+#
+#     def update
+#         @goal.update(goal_params)
+#         redirect_to goal_vacation_path(@goal)
+#     end
+#
+#     def update
+#         @goal.update(goal_params)
+#         redirect_to goal_vacation_path(@goal)
+#     end
+#
+#     def destroy
+#         @goal.destroy
+#         flash[:notice] = "Goal deleted."
+#         redirect_to goals_path
+#     end
+#
+    private
     def goal_params
-        params.require(:goal).permit(:amount, vacation_attributes:[:id, :description, :date_traveling, :user_id, :goal_id])
+        params.require(:goal).permit(:amount)
     end
 end

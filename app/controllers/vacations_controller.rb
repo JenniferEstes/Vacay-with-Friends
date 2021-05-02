a79d433946cf5688dd9ed5a65e69650005e49ebe
Jennifer <jenndestes@gmail.com>
2021-05-01 20:43:39 -0500

    Edited index method in vacations controller to show that vacations belong to groups

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -2,7 +2,7 @@ class VacationsController < ApplicationController
     def index
         if params[:group_id] && @group = Group.find_by_id(params[:group_id])
             redirect_to root_path unless @group.users.include? current_user
-            @vacations = Vacation.all
+            @vacations = @group.vacations
         else
             flash[:notice] = "That group doesn't exist!"
             redirect_to groups_path
@@ -36,6 +36,21 @@ class VacationsController < ApplicationController
     def show
         @vacation = Vacation.find_by_id(params[:id])
         redirect_to root_path unless @vacation.group.users.include? current_user
+    end
+
+    def edit
+        @vacation = Vacation.find_by_id(params[:id])
+        redirect_to root_path unless @vacation.group.users.include? current_user
+    end
+
+    def update
+        @vacation = Vacation.find_by_id(params[:id])
+        redirect_to root_path unless @vacation.group.users.include? current_user
+        if @vacation.update(vacation_params)
+            redirect_to group_vacations_path(@vacation.group)
+        else
+            render :edit
+        end
     end
 
 

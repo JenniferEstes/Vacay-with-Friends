4234c090e44a43dd0a486d7166aaf64c24652ad5
Jennifer <jenndestes@gmail.com>
2021-05-03 13:42:49 -0500

    Changed method from create_by to find_or_create_by.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,47 +1,48 @@
 class SessionsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:omniauth]
+  skip_before_action :verify_authenticity_token, only: [:omniauth]
 
-    # def home
-    #     end
-    # end
+  # def home
+  #     end
+  # end
 
-    def destroy
-        session.clear
-        redirect_to root_path
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  def new
+    if logged_in?
+      redirect_to user_path(current_user)
     end
-    
-    def new
-        if logged_in?
-            redirect_to user_path(current_user)
-        end
-    end
+  end
 
-    #to get email - request.env['omniauth.auth'][:info][:email]
-    def create
-        if params[:provider] == 'github'
-            @user = User.create_by_github_omniauth(auth)
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            @user = User.find_by(email: params[:user][:email])
-            if @user && @user.authenticate(params[:user][:password])
-                session[:user_id] = @user.id
-                redirect_to user_path(@user)
-            else
-                flash[:message] = "Invalid input. Please try again."
-                redirect_to login_path
-            end
-        end
-    end
-
-    def omniauth
-        @user = User.create_by_google_omniauth(auth)
+  #to get email - request.env['omniauth.auth'][:info][:email]
+  def create
+    if params[:provider] == 'github'
+      @user = User.find_or_create_by_github_omniauth(auth)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
+      else
+        flash[:message] = "Invalid input. Please try again."
+        redirect_to login_path
+      end
     end
+  end
 
-    private
-    def auth
-        request.env['omniauth.auth']
-    end
+  def omniauth
+    @user = User.create_by_google_omniauth(auth)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end

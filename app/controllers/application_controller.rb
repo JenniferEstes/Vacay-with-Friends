4687e4e32b46d2bd0dc697f9892b2c5b71fa8831
Jennifer <jenndestes@gmail.com>
2021-05-03 13:38:06 -0500

    Added class method to protect from CSRF.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
-#Can use in views
-helper_method :current_user, :logged_in?
- 
-    def current_user
-        @current_user || User.find_by_id(session[:user_id]) if session[:user_id]
-    end
+  protect_from_forgery with: :exception
 
-    def logged_in?
-        !!current_user
-    end
-    
-    def redirect_if_not_logged_in
-        redirect_to root_path if !logged_in?
-    end
+  #Can use in views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user || User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to root_path if !logged_in?
+  end
 end

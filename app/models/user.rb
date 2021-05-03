bab7abe83fb867799c9e98c9d8d029fb1ee6c63f
Jennifer <jenndestes@gmail.com>
2021-05-03 13:44:44 -0500

    Cannot validate presence of group name or group members anymore because github does not ask validate that information.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,26 @@
 class User < ApplicationRecord
-    has_many :vacations, through: :groups
-    has_many :goals
-    has_and_belongs_to_many :groups
+  has_many :vacations, through: :groups
+  has_many :goals
+  has_and_belongs_to_many :groups
 
-    #gives password validation
-    #gives reader and writer methods
-    #gives authenticate method
-    has_secure_password
+  #gives password validation
+  #gives reader and writer methods
+  #gives authenticate method
+  has_secure_password
 
-    validates :email, uniqueness: true
-    validates :email, :group_name, :group_members, :first_name, :last_name, presence: true
-    validates :password, length: { in: 3...20 }
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: { in: 3...128 }
 
-    def self.find_or_create_by_github_omniauth(auth)
-        self.find_or_create_by(email: auth[:info][:email]) do |u|
-            u.password = SecureRandom.hex
-        end
+  def self.find_or_create_by_github_omniauth(auth)
+    user = self.find_or_create_by(email: auth[:info][:email]) do |u|
+      u.password = SecureRandom.hex
     end
+    if !user.id && !user.save
+      flash[:notice] = 'Could not create an account using Github.'
+      redirect_to new_user_path
+    else
+      user
+    end
+  end
 end
 

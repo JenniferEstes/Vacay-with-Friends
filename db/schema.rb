c2e5f6d373f65aa812af4bd9e73d0ff2512ce36e
Jennifer <jenndestes@gmail.com>
2021-05-03 00:51:18 -0500

    Changed group ID from string to Integer in migration table

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_053328) do
     t.date "date_traveling"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "group_id"
+    t.integer "group_id"
   end
 
   add_foreign_key "goals", "users"

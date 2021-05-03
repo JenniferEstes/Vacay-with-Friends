4f50790b4f25c671b0261bb0b301826a5c9fdbdc
Jennifer <jenndestes@gmail.com>
2021-05-02 19:54:55 -0500

    Setting up omniauth and devise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    # skip_before_action :verify_authenticity_token, only: :create
+    skip_before_action :verify_authenticity_token, only: [:omniauth]
 
     # def home
     #     end
@@ -16,29 +16,28 @@ class SessionsController < ApplicationController
         end
     end
 
+    #to get email - request.env['omniauth.auth'][:info][:email]
     def create
-        user = User.find_by(email: params[:user][:email])
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+        if params[:provider] == 'github'
+            @user = User.create_by_github_omniauth(auth)
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
-            redirect_to login_path, notice: "Invalid information. Please try again."
+            @user = User.find_by(uid: params[:user][:uid])
+            if @user && @user.authenticate(password: params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:message] = "Invalid input. Please try again."
+                redirect_to login_path
+            end
         end
     end
 
-    #to get email - request.env['omniauth.auth'][:info][:email]
     def omniauth
-        user = User.find_or_create_by(uid: auth[:uid], provider: auth[:provider]) do |u|
-            u.email = auth[:user][:email]
-            u.password = SecureRandom.hex(15)
-        end
-        if user.valid?
-            session[:user_id] = user.id
-            redirect to user_path
-        else
-            flash[:message] = "Invalid input. Please try again."
-            redirect_to login_path
-        end
+        @user = User.create_by_google_omniauth(auth)
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
     end
 
     private

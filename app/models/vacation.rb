6a13def54bfe3613be4eb726cad6e91802bf6097
Jennifer <jenndestes@gmail.com>
2021-05-04 13:44:25 -0500

    Added scope method per project requirements.

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -6,4 +6,10 @@ class Vacation < ApplicationRecord
   validates :description, presence: true
   validates :date_traveling, presence: true
 
+  scope :most_recent, ->(group) { where('group_id = ?', group.id).where('date_traveling >= ?', DateTime.now).order(date_traveling: :asc) }
+
+  # def self.most_recent(group)
+  #   self.order(updated_at: :desc).where(group_id: group.id)
+  # end
+
 end

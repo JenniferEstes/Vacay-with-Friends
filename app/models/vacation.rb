fba1832388bf6a9b4c156e3f99fe2f25a29bc6c0
Jennifer <jenndestes@gmail.com>
2021-05-05 19:58:12 -0500

    Deleted notes because they're no longer needed

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,15 +1,13 @@
 class Vacation < ApplicationRecord
-  belongs_to :group
   has_many :goals
   has_many :users, through: :groups
+  belongs_to :group
 
   validates :description, presence: true
   validates :date_traveling, presence: true
 
   scope :most_recent, ->(group) { where('group_id = ?', group.id).where('date_traveling >= ?', DateTime.now).order(date_traveling: :asc) }
 
-  # def self.most_recent(group)
-  #   self.order(updated_at: :desc).where(group_id: group.id)
-  # end
+
 
 end
